b1392ba5f9775f402395d6a96302c9c20e08d706
Mateo_Cepeda <mateo.cepeda.bonilla@gmail.com>
2026-03-02 20:12:12 -0500

    Durante esta sesión hice un código que pide el nombre, la edad y la ciudad para dar un saludo personalizado. Implementé unos bucles while y un try-except para que el programa no se cierre si alguien escribe mal los datos o deja espacios en blanco, y así asegurarme de que la información sea correcta antes de mostrarla.

---
+++ 01_saludo_formateado.py
@@ -0,0 +1,4 @@
+nombre=input("¿Cuál es tu nombre? ").capitalize()
+edad=int(input("¿Cuál es tu edad? "))
+ciudad=input("¿En qué ciudad vives? ").capitalize()
+print(f"Hola, {nombre}, tienes {edad} años y vives en {ciudad}.")
